2480593df9b3e28baebbc96a2270c5ec6b8e04e9
Al-amin Yusuf <alaminyusuf131@gmail.com>
2021-05-15 16:47:22 +0100

    list app states

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+
+import * as tf from '@tensorflow/tfjs';
+import * as speech from '@tensorflow-models/speech-commands';
+
+// CSS file
 import './App.css';
 
 function App() {
+	// App States
+	const [model, setModel] = React.useState(null);
+	const [action, setAction] = React.useState(null);
+	const [label, setLabel] = React.useState(null);
+
 	return (
 		<React.Fragment>
 			<div className='app-header'>Niki Commands</div>
